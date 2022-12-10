0cbe84400a1c62b8fc5b6268d04fb459a517f677
Peter <plarochkin@gmail.com>
2022-12-11 02:22:16 +0300

    new

--- imperative.c
+++ imperative.c
@@ -221,7 +221,7 @@ int main(int argc, char** argv) {
     t = clock();
     
     
-    double epsilon = 0.000007;
+    double epsilon = 0.0000001;
     // const size_t M = 160;
     // const size_t N = 160;
     

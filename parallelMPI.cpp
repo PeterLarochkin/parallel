367e9285cb749d1f0b736a513a5022de0ced0b9d
Peter <plarochkin@gmail.com>
2022-12-11 16:54:52 +0300

    new

--- parallelMPI.cpp
+++ parallelMPI.cpp
@@ -881,7 +881,7 @@ int main(int argc, char** argv) {
     const size_t N = atoi(argv[2]);
     const size_t time_seq = atof(argv[3]);
     
-    double epsilon = 0.000004;    
+    double epsilon = 0.000005;    
     double A1 = 0.0;
     double A2 = 4.0;
     double B1 = 0.0;
